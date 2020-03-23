f0d929a9d47c38f016a86f153905c4fb2e874cbe
Patryk Węgrzyn <wegpat@gmail.com>
2020-03-23 02:50:58 +0100

    Black testing

--- notebooks/tools/data_loader.py
+++ notebooks/tools/data_loader.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def load_dat(path, as_ndarray=False, start=0, lines=512):
     result_list = []
-    with open(path, 'r') as fh:
+    with open(path, "r") as fh:
         for i, line in enumerate(fh):
             if i >= start and lines > 0:
                 result_list.append(tuple(line.split()))
